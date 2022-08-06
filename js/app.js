4788bd0d4cbe38d0f9e4b4fabded4b46c86f6ed3
Ernesto Puente <ernesto20puente@gmail.com>
2022-08-06 15:37:37 -0400

    Proyecto Mitad

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
     document.addEventListener('DOMContentLoaded',preguntarPresupuesto);
+
+    formulario.addEventListener('submit', agregarGasto);
 }
 
 
@@ -28,6 +30,28 @@ class UI{
         document.querySelector('#total').textContent = presupuesto;
         document.querySelector('#restante').textContent = restante;
     }
+
+    imprimirAlerta(mensaje, tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if (tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        }else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        //Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        //Insertar en el HTML
+        document.querySelector('.primario').insertBefore(divMensaje, formulario);
+
+        //Quitar del HTML
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
 }
 
 //Instanciar
@@ -47,4 +71,22 @@ function preguntarPresupuesto() {
     presupuesto = new Presupuesto(presupuestoUsuario);
     console.log(presupuesto);
     ui.insertarPresupuesto(presupuesto);
+}
+
+// Añade gastos
+function agregarGasto(e) {
+    e.preventDefault();
+
+    //Leer los datos del formulario
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    //Validar
+    if (nombre === '' || cantidad === '') {
+        ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+        return;
+    }else if(cantidad <= 0 || isNaN(cantidad)){
+        ui.imprimirAlerta('Cantidad no válida', 'error');
+        return;
+    }
 }
